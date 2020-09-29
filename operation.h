83f113c4f58e72b7a4c9c46185eb699168dd761d
Kalin8900 <Kalin8900@gmail.com>
2020-09-29 14:53:10 +0200

    Operation Class added

--- operation.h
+++ operation.h
@@ -3,23 +3,32 @@
 
 #include <QObject>
 #include <QDateTime>
-
+#include <QDataStream>
+#include <QDebug>
 
 class Operation : public QObject
 {
     Q_OBJECT
 public:
     Operation(const float& value, const QDateTime &date,
-              const QString &category, QObject *parent = nullptr);
+              const QString &category, const qint64 &number, QObject *parent = nullptr);
 
+    float value() const;
 
+    QDateTime date() const;
+
+    QString category() const;
+
+    friend QDataStream& operator<<(QDataStream &ds, const Operation &op);
+
+    friend QDataStream& operator>>(QDataStream &ds, Operation &op);
 
 
 private:
     float value_;
     QDateTime date_;
     QString category_;
-
+    qint64 number_;
 };
 
 #endif // OPERATION_H
