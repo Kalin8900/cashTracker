217a25b3e5033f44583313df8bbfc705ba6e1376
Kalin8900 <Kalin8900@gmail.com>
2020-09-29 18:40:26 +0200

    Unit tests added

--- operation.cpp
+++ operation.cpp
@@ -2,9 +2,8 @@
 
 
 Operation::Operation(const float &value, const QDateTime &date,
-                     const QString &category, const qint64 &number, QObject *parent)
-    : QObject(parent),
-      value_(value),
+                     const QString &category, const qint64 &number)
+    : value_(value),
       date_(date),
       category_(category),
       number_(number) {}
@@ -24,20 +23,25 @@ QString Operation::category() const
     return category_;
 }
 
+qint64 Operation::number() const
+{
+    return number_;
+}
+
 QDataStream &operator<<(QDataStream &ds, const Operation &op)
 {
-    qInfo() << "Saving Operation number " << op.number_ << "\n";
-
     ds << op.value_ << op.date_ << op.category_ << op.number_;
-
     return ds;
 }
 
 QDataStream &operator>>(QDataStream &ds, Operation &op)
 {
     ds >> op.value_ >> op.date_ >> op.category_ >> op.number_;
-
-    qInfo() << "Loaded Operation number " << op.number_ << "\n";
-
     return ds;
+}
+
+bool operator==(const Operation &lhs, const Operation &rhs)
+{
+    return lhs.date() == rhs.date() && lhs.value() == rhs.value()
+            && lhs.category() == rhs.category() && lhs.number() == rhs.number();
 }
