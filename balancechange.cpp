ead95d6ecd428b76bb535e553ce8481af855a0de
Kalin8900 <Kalin8900@gmail.com>
2020-10-04 10:10:52 +0200

    Changed addOperation in BalanceChange class from pure virtual to standard method and implemented it

--- balancechange.cpp
+++ balancechange.cpp
@@ -4,6 +4,11 @@ BalanceChange::BalanceChange(QVector<Operation> ops)
     : operations(std::move(ops))
 {}
 
+void BalanceChange::addOperation(const Operation &operation)
+{
+    operations.push_back(operation);
+}
+
 Operation BalanceChange::removeLast()
 {
     return operations.takeLast();
