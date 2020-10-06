f0e63b9ad2fb7d0b505083deedfc7a975f927627
Kalin8900 <Kalin8900@gmail.com>
2020-10-06 15:24:17 +0200

    Basic functionality added along with members and method

--- balance.cpp
+++ balance.cpp
@@ -1,6 +1,25 @@
 #include "balance.h"
 
-Balance::Balance(QObject *parent) : QObject(parent)
-{
+Balance::Balance(const float &balance, const BalanceChange &income,
+                 const BalanceChange &expense, QObject *parent)
+    : QObject(parent),
+      income_(income),
+      expense_(expense),
+      balance_(balance) {}
 
+void Balance::changeBalance(const Operation &op)
+{
+    balance_ += op.value();
+
+    (op.value() > 0) ? income_.addOperation(op) : expense_.addOperation(op);
+}
+
+BalanceChange Balance::income() const
+{
+    return income_;
+}
+
+BalanceChange Balance::expense() const
+{
+    return expense_;
 }
--- balance.h
+++ balance.h
@@ -2,15 +2,24 @@
 #define BALANCE_H
 
 #include <QObject>
+#include "balancechange.h"
 
 class Balance : public QObject
 {
     Q_OBJECT
 public:
-    explicit Balance(QObject *parent = nullptr);
+    Balance(const float &balance, const BalanceChange &income,
+                     const BalanceChange &expense, QObject *parent = nullptr);
 
-signals:
+    void changeBalance(const Operation &op);
 
+    BalanceChange income() const;
+
+    BalanceChange expense() const;
+
+private:
+    BalanceChange income_, expense_;
+    float balance_;
 };
 
 #endif // BALANCE_H
