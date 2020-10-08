18a49d30d5956e38f3ab9af23dd923b9260aea82
Kalin8900 <Kalin8900@gmail.com>
2020-10-08 12:26:24 +0200

    Simple appregister functionality

--- appregister.cpp
+++ appregister.cpp
@@ -2,5 +2,28 @@
 
 AppRegister::AppRegister()
 {
+    AppRegister::init();
+}
+
+void AppRegister::init()
+{
+    //path
+    const QString path = QDir::currentPath() + QDir::separator() + "appregisterinit.txt";
+
+    //check if exists
+
+
+    QFile file(path);
+    QDataStream ts(&file);
+    bool read;
+    ts >> read;
+
+    firstTime = read;
+    //file
+    //read from file
+    //firstTime
+    //save to class
+
+
 
 }
--- appregister.h
+++ appregister.h
@@ -2,12 +2,21 @@
 #define APPREGISTER_H
 
 #include <QObject>
+#include <QDir>
+#include <QFile>
+#include <QDataStream>
+#include <QTextStream>
+
 
 class AppRegister
 {
 public:
     AppRegister();
 
+private:
+    bool firstTime;
+
+    void init();
 };
 
 #endif // APPREGISTER_H
