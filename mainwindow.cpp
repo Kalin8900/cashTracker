b7e35aed88fefe21a1d3fa3019188f8fd60b3996
Kalin8900 <Kalin8900@gmail.com>
2020-10-28 21:58:10 +0100

    Added basic layout of the app

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,16 +2,56 @@
 #include "./ui_mainwindow.h"
 
 #include <QMessageBox>
+#include <QtQuickWidgets/QQuickWidget>
 #include <QDebug>
 
+void setTextInCenter(QLabel *l, const QString &msg)
+{
+    l->setText(msg);
+    l->setAlignment(Qt::AlignCenter);
+}
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow),
+      timer_(new QTimer(this))
 {
     ui->setupUi(this);
+    connect(timer_, &QTimer::timeout, this, QOverload<>::of(&MainWindow::updateTimer));
+    timer_->start(1000);
+
+    setTextInCenter(ui->date, QDate::currentDate().toString("dd-MM-yyyy"));
+    setTextInCenter(ui->pageName, "Home");
+
+    lastOperationLabels.at(0) = ui->lastAmount;
+    lastOperationLabels.at(1) = ui->lastDate;
+    lastOperationLabels.at(2) = ui->lastCategory;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->pages->setCurrentIndex(0);
+    setTextInCenter(ui->pageName, "Home");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->pages->setCurrentIndex(1);
+    setTextInCenter(ui->pageName, "Income");
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->pages->setCurrentIndex(2);
+    setTextInCenter(ui->pageName, "Expense");
+}
+
+void MainWindow::updateTimer()
+{
+    setTextInCenter(ui->time, QTime::currentTime().toString("hh:mm"));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTime>
+#include <QDate>
+#include <QTimer>
+#include <QLabel>
+#include <array>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void updateTimer();
+
 private:
     Ui::MainWindow *ui;
+
+    std::array<QLabel *, 3> lastOperationLabels; //amount, date, category
+
+    QTimer *timer_;
 };
 #endif // MAINWINDOW_H
